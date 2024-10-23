3e5967eed541c414e3985fdee9e0e9255398d8dc
Andrew Kishino <akishino@twilio.com>
2024-10-23 11:54:51 -0700

    Fix Terraform config variables

--- infra-as-code/terraform/modules/conversation-transfer/variables.tf
+++ infra-as-code/terraform/modules/conversation-transfer/variables.tf
@@ -97,11 +97,11 @@ variable "support_chat_phone_numbers_queue_sid" {
   }
 }
 
-variable "suppor_chat_voice_queue_sid" {
+variable "support_chat_voice_queue_sid" {
   type        = string
-  description = "SID of suppor_chat_voice queue"
+  description = "SID of support_chat_voice queue"
   validation {
-    condition     = length(var.suppor_chat_voice_queue_sid) > 2 && substr(var.suppor_chat_voice_queue_sid, 0, 2) == "WQ"
-    error_message = "suppor_chat_voice_queue_sid expected to start with \"WQ\"."
+    condition     = length(var.support_chat_voice_queue_sid) > 2 && substr(var.support_chat_voice_queue_sid, 0, 2) == "WQ"
+    error_message = "support_chat_voice_queue_sid expected to start with \"WQ\"."
   }
 }
