1ff9bc3966c02900a4aa47f642f2e66986fdaa13
Andrew Kishino <akishino@twilio.com>
2024-10-24 15:38:12 -0700

    Fix retry logic

--- infra-as-code/state/import_internal_state.sh
+++ infra-as-code/state/import_internal_state.sh
@@ -104,11 +104,12 @@ retries=3
 count=0
 delay=5
 
-### terraform apply may fail due to rate limits when creating new resources
-### retrying if it fails
+### terraform apply may fail due to rate limits when creating new resources. Retrying if it fails
 while [ $count -lt $retries ]; do
-	terraform -chdir="../terraform/environments/default" apply -input=false -auto-approve -var-file="${ENVIRONMENT:-local}.tfvars"
-	if [ $? -eq 0 ]; then
+	terraform -chdir="../terraform/environments/default" apply -input=false -auto-approve -var-file="${ENVIRONMENT:-local}.tfvars" || true
+	exit_status=$?
+
+	if [ exit_status -eq 0 ]; then
 		echo " - Applying terraform configuration complete" >>$GITHUB_STEP_SUMMARY
 		echo "JOB_FAILED=false" >>"$GITHUB_OUTPUT"
 		exit 0
