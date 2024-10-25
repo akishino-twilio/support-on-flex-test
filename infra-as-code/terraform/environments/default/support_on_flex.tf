1a0ff63e5374cc9dbc901ece1afdb6f43ae8894e
Andrew Kishino <akishino@twilio.com>
2024-10-25 10:05:07 -0700

    Update friendly names and clean up boilerplate

--- infra-as-code/terraform/environments/default/support_on_flex.tf
+++ infra-as-code/terraform/environments/default/support_on_flex.tf
@@ -1,40 +1,4 @@
-# FEATURE: remove-all
-resource "twilio_taskrouter_workspaces_task_queues_v1" "template_example_everyone" {
-  workspace_sid  = var.TWILIO_FLEX_WORKSPACE_SID
-  friendly_name  = "Template Example Everyone"
-  target_workers = "1==1"
-  max_reserved_workers = 1
-  task_order = "FIFO"
-}
-
-resource "twilio_taskrouter_workspaces_task_queues_v1" "template_example_sales" {
-  workspace_sid  = var.TWILIO_FLEX_WORKSPACE_SID
-  friendly_name  = "Template Example Sales"
-  target_workers = "routing.skills HAS 'template_example_sales'"
-  max_reserved_workers = 1
-  task_order = "FIFO"
-}
-
-resource "twilio_taskrouter_workspaces_task_queues_v1" "template_example_support" {
-  workspace_sid  = var.TWILIO_FLEX_WORKSPACE_SID
-  friendly_name  = "Template Example Support"
-  target_workers = "routing.skills HAS 'template_example_support'"
-  max_reserved_workers = 1
-  task_order = "FIFO"
-}
-
-resource "twilio_taskrouter_workspaces_workflows_v1" "template_example_assign_to_anyone" {
-  workspace_sid = var.TWILIO_FLEX_WORKSPACE_SID
-  friendly_name = "Template Example Assign to Anyone"
-  configuration = templatefile("workflows/template_example_assign_to_anyone.json", {
-    "QUEUE_SID_EVERYONE" = twilio_taskrouter_workspaces_task_queues_v1.template_example_everyone.sid
-    "QUEUE_SID_TEMPLATE_EXAMPLE_SALES" = twilio_taskrouter_workspaces_task_queues_v1.template_example_sales.sid
-    "QUEUE_SID_TEMPLATE_EXAMPLE_SUPPORT" = twilio_taskrouter_workspaces_task_queues_v1.template_example_support.sid
-  })
-}
-# END FEATURE: remove-all
-
-### Support on Flex resources ###
+### Support on Flex Task Queues ###
 resource "twilio_taskrouter_workspaces_task_queues_v1" "support_chat_live_chat" {
   workspace_sid  = var.TWILIO_FLEX_WORKSPACE_SID
   friendly_name  = "Live Chat (Everyone)"
@@ -45,7 +9,7 @@ resource "twilio_taskrouter_workspaces_task_queues_v1" "support_chat_live_chat" 
 
 resource "twilio_taskrouter_workspaces_task_queues_v1" "support_chat_accounts_billing" {
   workspace_sid  = var.TWILIO_FLEX_WORKSPACE_SID
-  friendly_name  = "Accounts Billing"
+  friendly_name  = "Account Administration"
   target_workers = "routing.skills HAS 'support_accounts_billing'"
   max_reserved_workers = 1
   task_order = "FIFO"
@@ -123,6 +87,7 @@ resource "twilio_taskrouter_workspaces_task_queues_v1" "support_voicemail_person
   task_order = "FIFO"
 }
 
+### Support on Flex Workflows ###
 resource "twilio_taskrouter_workspaces_workflows_v1" "support_chat_live_chat" {
   workspace_sid = var.TWILIO_FLEX_WORKSPACE_SID
   friendly_name = "Support Live Chat"
--- infra-as-code/terraform/modules/conversation-transfer/main.tf
+++ infra-as-code/terraform/modules/conversation-transfer/main.tf
@@ -9,12 +9,8 @@ terraform {
 
 resource "twilio_taskrouter_workspaces_workflows_v1" "live_chat_transfer" {
   workspace_sid = var.workspace_sid
-  friendly_name = "Live Chat Transfers"
+  friendly_name = "Live Chat Transfer"
   configuration = templatefile("${path.module}/workflows/live_chat_transfer.json", {
-    "QUEUE_SID_EVERYONE" = var.everyone_queue_sid
-    "QUEUE_SID_TEMPLATE_EXAMPLE_SALES" = var.example_sales_queue_sid
-    "QUEUE_SID_TEMPLATE_EXAMPLE_SUPPORT" = var.example_support_queue_sid
-
     "QUEUE_SID_SUPPORT_CHAT_LIVE_CHAT" = var.support_chat_live_chat_queue_sid
     "QUEUE_SID_SUPPORT_CHAT_ACCOUNTS_BILLING" = var.support_chat_accounts_billing_queue_sid
     "QUEUE_SID_SUPPORT_CHAT_BILLING" = var.support_chat_billing_queue_sid
