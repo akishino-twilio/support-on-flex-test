2a08d4c8553f198af8ff49ee882b995f900fbd94
Andrew Kishino <akishino@twilio.com>
2024-10-25 15:21:32 -0700

    Update more

--- infra-as-code/terraform/modules/conversation-transfer/variables.tf
+++ infra-as-code/terraform/modules/conversation-transfer/variables.tf
@@ -21,7 +21,7 @@ variable "support_accounts_billing_queue_sid" {
   description = "SID of support_accounts_billing queue"
   validation {
     condition     = length(var.support_accounts_billing_queue_sid) > 2 && substr(var.support_accounts_billing_queue_sid, 0, 2) == "WQ"
-    error_message = "support_chat_accounts_billing_queue_sid expected to start with \"WQ\"."
+    error_message = "support_accounts_billing_queue_sid expected to start with \"WQ\"."
   }
 }
 
@@ -30,7 +30,7 @@ variable "support_billing_queue_sid" {
   description = "SID of support_billing queue"
   validation {
     condition     = length(var.support_billing_queue_sid) > 2 && substr(var.support_billing_queue_sid, 0, 2) == "WQ"
-    error_message = "support_chat_billing_queue_sid expected to start with \"WQ\"."
+    error_message = "support_billing_queue_sid expected to start with \"WQ\"."
   }
 }
 
@@ -39,7 +39,7 @@ variable "support_email_queue_sid" {
   description = "SID of support_email queue"
   validation {
     condition     = length(var.support_email_queue_sid) > 2 && substr(var.support_email_queue_sid, 0, 2) == "WQ"
-    error_message = "support_chat_email_queue_sid expected to start with \"WQ\"."
+    error_message = "support_email_queue_sid expected to start with \"WQ\"."
   }
 }
 
@@ -48,7 +48,7 @@ variable "support_messaging_queue_sid" {
   description = "SID of support_messaging queue"
   validation {
     condition     = length(var.support_messaging_queue_sid) > 2 && substr(var.support_messaging_queue_sid, 0, 2) == "WQ"
-    error_message = "support_chat_messaging_queue_sid expected to start with \"WQ\"."
+    error_message = "support_messaging_queue_sid expected to start with \"WQ\"."
   }
 }
 
@@ -57,7 +57,7 @@ variable "support_messaging_insights_queue_sid" {
   description = "SID of support_messaging_insights queue"
   validation {
     condition     = length(var.support_messaging_insights_queue_sid) > 2 && substr(var.support_messaging_insights_queue_sid, 0, 2) == "WQ"
-    error_message = "support_chat_messaging_insights_queue_sid expected to start with \"WQ\"."
+    error_message = "support_messaging_insights_queue_sid expected to start with \"WQ\"."
   }
 }
 
@@ -66,7 +66,7 @@ variable "support_phone_numbers_queue_sid" {
   description = "SID of support_phone_numbers queue"
   validation {
     condition     = length(var.support_phone_numbers_queue_sid) > 2 && substr(var.support_phone_numbers_queue_sid, 0, 2) == "WQ"
-    error_message = "support_chat_phone_numbers_queue_sid expected to start with \"WQ\"."
+    error_message = "support_phone_numbers_queue_sid expected to start with \"WQ\"."
   }
 }
 
@@ -75,6 +75,6 @@ variable "support_voice_queue_sid" {
   description = "SID of support_voice queue"
   validation {
     condition     = length(var.support_voice_queue_sid) > 2 && substr(var.support_voice_queue_sid, 0, 2) == "WQ"
-    error_message = "support_chat_voice_queue_sid expected to start with \"WQ\"."
+    error_message = "support_voice_queue_sid expected to start with \"WQ\"."
   }
 }
