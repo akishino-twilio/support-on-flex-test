2b18e4c33416d336e18901aaf4576550b2c4ac52
Andrew Kishino <akishino@twilio.com>
2024-10-25 13:37:30 -0700

    remove leading _

--- infra-as-code/terraform/modules/park-interaction/outputs.tf
+++ infra-as-code/terraform/modules/park-interaction/outputs.tf
@@ -1,9 +1,9 @@
 output "park_resume_workflow_sid" {
-  value = twilio_taskrouter_workspaces_workflows_v1._park_resume.sid
+  value = twilio_taskrouter_workspaces_workflows_v1.park_resume.sid
   description = "Park/resume workflow SID"
 }
 
 output "parking_flow_sid" {
-  value = twilio_studio_flows_v2._messaging_with_parking_flow.sid
+  value = twilio_studio_flows_v2.messaging_with_parking_flow.sid
   description = "Messaging with parking flow SID"
 }
