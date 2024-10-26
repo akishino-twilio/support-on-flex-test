54da06999a9e32d3e07e78487f0a1dc81cfdc3e0
Andrew Kishino <akishino@twilio.com>
2024-10-26 15:24:02 -0700

    Add ivr workflow

--- infra-as-code/terraform/modules/personalized-ivr/main.tf
+++ infra-as-code/terraform/modules/personalized-ivr/main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+resource "twilio_taskrouter_workspaces_workflows_v1" "support_voice_personalized" {
+  workspace_sid = var.workspace_sid
+  friendly_name = "support_voice_personalized"
+  configuration = templatefile("${path.module}/workflows/support_voice_personalized.json", {
+    "QUEUE_SID_PERSONALIZED_TAM" = var.support_personalized_tam_queue_sid
+    "QUEUE_SID_PERSONALIZED_SUPPORT_ENGINEER" = var.support_personalized_support_engineer_queue_sid
+  })
+}
+
 resource "twilio_studio_flows_v2" "personalized_ivr_flow" {
   friendly_name  = "support-personalized"
   status         = "published"
--- infra-as-code/terraform/modules/personalized-ivr/variables.tf
+++ infra-as-code/terraform/modules/personalized-ivr/variables.tf
@@ -1,8 +1,35 @@
+variable "workspace_sid" {
+  type        = string
+  description = "TaskRouter workspace SID"
+  validation {
+    condition     = length(var.workspace_sid) > 2 && substr(var.workspace_sid, 0, 2) == "WS"
+    error_message = "workspace_sid expected to start with \"WS\"."
+  }
+}
+
 variable "support_authorizer_domain" {
   type        = string
   description = "support authorizer domain for studio proxy"
   validation {
     condition     = length(var.support_authorizer_domain) > 36 && substr(var.support_authorizer_domain, 10, 46) == ".execute-api.us-east-1.amazonaws.com"
     error_message = "support_authorizer_domain expected to end with with \".execute-api.us-east-1.amazonaws.com\"."
+  }
+}
+
+variable "support_personalized_tam_queue_sid" {
+  type        = string
+  description = "SID of support_personalized_tam queue"
+  validation {
+    condition     = length(var.support_personalized_tam_queue_sid) > 2 && substr(var.support_personalized_tam_queue_sid, 0, 2) == "WQ"
+    error_message = "support_personalized_tam_queue_sid expected to start with \"WQ\"."
+  }
+}
+
+variable "support_personalized_support_engineer_queue_sid" {
+  type        = string
+  description = "SID of support_personalized_support_engineer queue"
+  validation {
+    condition     = length(var.support_personalized_support_engineer_queue_sid) > 2 && substr(var.support_personalized_support_engineer_queue_sid, 0, 2) == "WQ"
+    error_message = "support_personalized_support_engineer_queue_sid expected to start with \"WQ\"."
   }
 }
